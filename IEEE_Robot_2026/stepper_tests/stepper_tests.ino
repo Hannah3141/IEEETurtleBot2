de20e26a57baf39ce823f2f08b981fe0572312f6
Alice <nadellepav@gmail.com>
2026-02-10 19:14:09 -0600

    code for new type of stepper

--- IEEE_Robot_2026/stepper_tests/stepper_tests.ino
+++ IEEE_Robot_2026/stepper_tests/stepper_tests.ino
@@ -1,78 +1,27 @@
-#define MOTOR_1_IN1 2
-#define MOTOR_1_IN2 3
-#define MOTOR_1_IN3 4
-#define MOTOR_1_IN4 5
-
-int stepPhase = 0;
+#define STEP_1 2
+#define DIR_1 3
 
 void setup() {
-  pinMode(MOTOR_1_IN1, OUTPUT);
-  digitalWrite(MOTOR_1_IN1, LOW);
-
-  pinMode(MOTOR_1_IN2, OUTPUT);
-  digitalWrite(MOTOR_1_IN2, LOW);
-
-  pinMode(MOTOR_1_IN3, OUTPUT);
-  digitalWrite(MOTOR_1_IN3, LOW);
-
-  pinMode(MOTOR_1_IN4, OUTPUT);
-  digitalWrite(MOTOR_1_IN4, LOW);
+  pinMode(STEP_1, OUTPUT);
+  pinMode(DIR_1, OUTPUT);
 }
 
 void loop() {
-  for (int i = 0; i < 100; i++) {
-    motorstep(1);
-    delay(5);
-  }
+  motorstep(10, 1);
+  delay(5);
+  motorstep(10, 0);
 }
 
-void motorstep(int direction) {
-  //
-  // compute the next phase number
-  //
-  //stepPhase += (-direction);
-
-  if (stepPhase <= -1)
-    stepPhase = 3;
-
-  if (stepPhase >= 4)
-    stepPhase = 0;
-
-  //
-  // set the coils for this phase
-  //
-  switch (stepPhase) {
-    case 0:
-      digitalWrite(MOTOR_1_IN1, LOW);
-      digitalWrite(MOTOR_1_IN2, LOW);
-      digitalWrite(MOTOR_1_IN3, HIGH);
-      digitalWrite(MOTOR_1_IN4, HIGH);
-      break;
-    case 1:
-      digitalWrite(MOTOR_1_IN1, LOW);
-      digitalWrite(MOTOR_1_IN2, HIGH);
-      digitalWrite(MOTOR_1_IN3, HIGH);
-      digitalWrite(MOTOR_1_IN4, LOW);
-      break;
-    case 2:
-      digitalWrite(MOTOR_1_IN1, HIGH);
-      digitalWrite(MOTOR_1_IN2, HIGH);
-      digitalWrite(MOTOR_1_IN3, LOW);
-      digitalWrite(MOTOR_1_IN4, LOW);
-      break;
-    case 3:
-      digitalWrite(MOTOR_1_IN1, HIGH);
-      digitalWrite(MOTOR_1_IN2, LOW);
-      digitalWrite(MOTOR_1_IN3, LOW);
-      digitalWrite(MOTOR_1_IN4, HIGH);
-      break;
+void motorStep(int numSteps, int direction) {
+  if (direction == 0) {
+    digitalWrite(DIR_1, LOW);
+  } else {
+    digitalWrite(DIR_1, HIGH);
   }
-  stepPhase++;
-}
-
-void disableMotor() {
-  digitalWrite(MOTOR_1_IN1, LOW);
-  digitalWrite(MOTOR_1_IN2, LOW);
-  digitalWrite(MOTOR_1_IN3, LOW);
-  digitalWrite(MOTOR_1_IN4, LOW);
+  for (int x = 0; x < numSTEPS; x++) {
+    digitalWrite(STEP_1, HIGH);
+    delayMicroseconds(700);  // by changing this time delay between the steps we can change the rotation speed
+    digitalWrite(STEP_1, LOW);
+    delayMicroseconds(700);
+  }
 }
