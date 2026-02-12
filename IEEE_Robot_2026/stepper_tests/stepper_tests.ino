9c2dd61b328d3993483d7a1de9f01e7100739058
Alice <nadellepav@gmail.com>
2026-02-12 09:29:06 -0600

    servo tester code

--- IEEE_Robot_2026/stepper_tests/stepper_tests.ino
+++ IEEE_Robot_2026/stepper_tests/stepper_tests.ino
@@ -1,15 +1,32 @@
-#define STEP_1 2
-#define DIR_1 3
+#include <Arduino.h>
+#include <Servo.h>
+
+// Stepper Motor
+#define ENA_1 2
+#define STEP_1 3
+#define DIR_1 4
+
+// Servos
+Servo leftServo;
+Servo rightServo;
+int leftPos = 0;
+int rightPos = 0;
 
 void setup() {
+  pinMode(ENA_1, OUTPUT);
   pinMode(STEP_1, OUTPUT);
   pinMode(DIR_1, OUTPUT);
+
+  leftServo.attach(8);
+  rightServo.attach(9);
 }
 
 void loop() {
   motorstep(10, 1);
+  turnServos(0);
   delay(5);
   motorstep(10, 0);
+  turnServos(1);
 }
 
 void motorStep(int numSteps, int direction) {
@@ -23,5 +40,24 @@ void motorStep(int numSteps, int direction) {
     delayMicroseconds(700);  // by changing this time delay between the steps we can change the rotation speed
     digitalWrite(STEP_1, LOW);
     delayMicroseconds(700);
+  }
+}
+
+void turnServos(int direction) {
+  if (direction == 0) {
+    rightPos = 180;
+    for (leftPos = 0, leftPos < 180; leftPos++) {
+      leftServo.write(leftPos);
+      rightServo.write(rightPos);
+      rightPos--;
+      delay(15);
+    }
+  } else {
+    for (rightPos = 0; rightPos < 180; rightPos++) {
+      leftServo.write(leftPos);
+      rightServo.write(rightPos);
+      leftPos--;
+      delay(15);
+    }
   }
 }
