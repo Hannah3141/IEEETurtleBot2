b6ba5cd7784d3c53525144c87f8b8bd8fb0bab6f
Alice <nadellepav@gmail.com>
2026-02-17 17:35:23 -0600

    changed bytes received

--- IEEE_Robot_2026/IEEE_Robot_2026.ino
+++ IEEE_Robot_2026/IEEE_Robot_2026.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 
 // Relay for Electromagnet
-#define RELAY_PIN A6
+#define RELAY_PIN 7
 
 //Stepper Motors
 #define ENA_1 2
@@ -31,69 +31,73 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available() >= 3) {  // command byte + 2 data bytes
-    uint8_t cmd = Serial.read();
-    uint8_t data1 = Serial.read();
-    uint8_t data2 = Serial.read();
+  if (Serial.available()) {  // command byte + 2 data bytes
+    if (Serial.read() == 0xFF) {
+      while (Serial.available() < 3)
+        ;
+      uint8_t cmd = Serial.read();
+      uint8_t data1 = Serial.read();
+      uint8_t data2 = Serial.read();
 
-    switch (cmd) {
-      case 0x01:  // Shovel Stepper
-        motorstep(data1, data2);  // 0 = down, 1 = up
-        Serial.write(0xAA);
-        break;
-      case 0x02:  // Relay
-        if (data1 == 0x00) {
-          digitalWrite(RELAY_PIN, LOW);  // OFF (active-low)
+      switch (cmd) {
+        case 0x01:                  // Shovel Stepper
+          motorstep(data1, data2);  // 0 = down, 1 = up
           Serial.write(0xAA);
-        } else if (data1 == 0x01) {
-          digitalWrite(RELAY_PIN, HIGH);  // ON (active-low)
+          break;
+        case 0x02:  // Relay
+          if (data1 == 0x00) {
+            digitalWrite(RELAY_PIN, LOW);  // OFF (active-low)
+            Serial.write(0xAA);
+          } else if (data1 == 0x01) {
+            digitalWrite(RELAY_PIN, HIGH);  // ON (active-low)
+            Serial.write(0xAA);
+          } else {
+            Serial.write(0xFF);
+          }
+          break;
+        case 0x03:  // Servos
+          turnServos(data1);
           Serial.write(0xAA);
-        } else {
-          Serial.write(0xFF);
-        }
-        break;
-      case 0x03: // Servos
-        turnServos(data1);
-        Serial.write(0xAA);
-        break;
-      default:
-        Serial.write(0xFF);  // ERROR: unknown command
-        break;
+          break;
+        default:
+          Serial.write(0xFF);  // ERROR: unknown command
+          break;
+      }
     }
   }
 }
 
-void motorstep(int numSteps, int direction) {
-  if (direction == 0) {  // move down
-    digitalWrite(DIR_1, LOW);
-  } else {  // move up
-    digitalWrite(DIR_1, HIGH);
-  }
-
-  for (int i = 0; i < numSteps; i++) {
-    digitalWrite(STEP_1, HIGH);
-    delay(1);
-    digitalWrite(STEP_1, LOW);
-    delay(1);
-  }
-}
-
-void turnServos(int direction) {
-  if (direction == 0) {
-    rightPos = 180;
-    for (leftPos = 0; leftPos < 180; leftPos++) {
-      leftServo.write(leftPos);
-      rightServo.write(rightPos);
-      rightPos--;
-      delay(15);
+  void motorstep(int numSteps, int direction) {
+    if (direction == 0) {  // move down
+      digitalWrite(DIR_1, LOW);
+    } else {  // move up
+      digitalWrite(DIR_1, HIGH);
     }
-  } else {
-    leftPos = 180;
-    for (rightPos = 0; rightPos < 180; rightPos++) {
-      leftServo.write(leftPos);
-      rightServo.write(rightPos);
-      leftPos--;
-      delay(15);
+
+    for (int i = 0; i < numSteps; i++) {
+      digitalWrite(STEP_1, HIGH);
+      delay(1);
+      digitalWrite(STEP_1, LOW);
+      delay(1);
     }
   }
-}
+
+  void turnServos(int direction) {
+    if (direction == 0) {
+      rightPos = 180;
+      for (leftPos = 0; leftPos < 180; leftPos++) {
+        leftServo.write(leftPos);
+        rightServo.write(rightPos);
+        rightPos--;
+        delay(15);
+      }
+    } else {
+      leftPos = 180;
+      for (rightPos = 0; rightPos < 180; rightPos++) {
+        leftServo.write(leftPos);
+        rightServo.write(rightPos);
+        leftPos--;
+        delay(15);
+      }
+    }
+  }
